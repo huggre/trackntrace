e772b9d4bb7deed956e8424051f279f53cbca48e
huggre <hgreg@online.no>
2020-06-12 10:18:43 +0200

    new commit

--- send_trans.py
+++ send_trans.py
@@ -7,10 +7,10 @@ seed = 'FYKVVGEI9AAZLLCFWYFFBG9SYRML9BENNGHYLQNMSXBRLXAERCPXOMQFBER9BDYIWMTYRSAI
 
 # Address = PGGRIBEL9NGEXEMCJRTYZ9UOTWGZBJWUSAASLYVLTRVPEYEFJRSX9DY9KSWHXY9ZWRZGMNUM9DGNUTYXWNBVQRBXEZ
 
-barcode_ID = '2234567898765'
+barcode_ID = '3234567898765'
 
 # Create upload json
-udata = {'Barcode ID' : '1234567898765', 'Transaction Type' : 'Inbound', 'Actor Name' : 'Bob the fisherman'}
+udata = {'barcode_ID' : '3234567898765', 'transaction_type' : 'Inbound', 'actor_name' : 'Bob the fisherman'}
 msg = json.dumps(udata)
 
 send_transaction(seed, barcode_ID, msg)
