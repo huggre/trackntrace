1d45b13c14aaa07e7a2b37fbf5befd69bba22cc2
huggre <hgreg@online.no>
2020-06-12 10:25:38 +0200

    new commit

--- send_trans.py
+++ send_trans.py
@@ -2,6 +2,7 @@
 import json
 
 from iota_interact import send_transaction
+from iota_interact import GenerateAddressFromBarcode
 
 seed = 'FYKVVGEI9AAZLLCFWYFFBG9SYRML9BENNGHYLQNMSXBRLXAERCPXOMQFBER9BDYIWMTYRSAIUZAEUZLUV'
 
@@ -9,10 +10,12 @@ seed = 'FYKVVGEI9AAZLLCFWYFFBG9SYRML9BENNGHYLQNMSXBRLXAERCPXOMQFBER9BDYIWMTYRSAI
 
 barcode_ID = '3234567898765'
 
+addr =  GenerateAddressFromBarcode(barcode_ID)
+
 # Create upload json
 udata = {'barcode_ID' : '3234567898765', 'transaction_type' : 'Inbound', 'actor_name' : 'Bob the fisherman'}
 msg = json.dumps(udata)
 
-send_transaction(seed, barcode_ID, msg)
+send_transaction(seed, addr, msg)
 
 print('Done')
